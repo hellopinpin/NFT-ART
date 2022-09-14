33ad47fe42a0624279d87d69313e99f9bf0e8164
hellopinpin <caeop211@gmail.com>
2022-09-14 23:39:28 +0800

    commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,13 +2,13 @@ var swiper = new Swiper(".artistSwiper", {
     cssMode: true,
     centeredSlides: true,
 
-    // navigation: {
-    //     nextEl: ".swiper-button-next",
-    //     prevEl: ".swiper-button-prev",
-    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
     pagination: {
         el: ".swiper-pagination",
-        type: "bullets",
+        // type: "bullets",
         clickable: true,
         // clickableClass: '.swiper-slide',
 
@@ -16,10 +16,10 @@ var swiper = new Swiper(".artistSwiper", {
     mousewheel: true,
     keyboard: true,
     breakpoints: {
-        // 992: {
-        //     slidesPerView: 2,
-        //     spaceBetween: 24,
-        //   },
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+          },
         1200: {
             slidesPerView: 3,
             spaceBetween: 24,
