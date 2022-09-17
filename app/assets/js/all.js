e84b9d64c0dd61f3ea839962ea43c47683488322
hellopinpin <caeop211@gmail.com>
2022-09-18 01:43:32 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,28 +1,57 @@
 var swiper = new Swiper(".artistSwiper", {
-    cssMode: true,
+    // cssMode: true,
     centeredSlides: true,
-
+    
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     pagination: {
         el: ".swiper-pagination",
-        // type: "bullets",
         clickable: true,
-        // clickableClass: '.swiper-slide',
-
     },
     mousewheel: true,
     keyboard: true,
+    // simulateTouch: true,
     breakpoints: {
         992: {
             slidesPerView: 2,
             spaceBetween: 24,
-          },
+            // simulateTouch: false,
+        },
         1200: {
             slidesPerView: 3,
             spaceBetween: 24,
+        },
+        
+    },
+});
+
+var swiper = new Swiper(".otherArtworksSwiper", {
+    // cssMode: true,
+    // loop: true,
+    // loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+            slidesPerGroup: 2,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+            slidesPerGroup: 4,
         },
         
     },
@@ -34,14 +63,21 @@ $(document).ready(function () {
         $(this).toggleClass('bg-warning');
         $(this).children('i').toggleClass('fa-plus').toggleClass('fa-minus');
     });
-
+    
     $('.nav-searchBar-mobile-show').click(function (e) { 
         e.preventDefault();
         $('.nav-searchBar-mobile').addClass('show');
     });
-
+    
     $('.nav-searchBar-mobile-hide').click(function (e) { 
         e.preventDefault();
         $('.nav-searchBar-mobile').removeClass('show');
     });
 });
+
+// var $grid = $('.masonry').imagesLoaded( function() {
+//     // init Masonry after all images have loaded
+//     // $grid.masonry({
+//     //   // options...
+//     // });
+// });
